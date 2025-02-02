85c463e86686c2eb8d47eb87c363178fe00a6be7
misollae <leticia.isb.lucas@gmail.com>
2025-02-02 21:57:46 +0000

    🌒 Now With RigidBodies and De-Spawning!

--- Assets/Scripts/Vision/Spawnable.cs
+++ Assets/Scripts/Vision/Spawnable.cs
@@ -4,13 +4,21 @@ using UnityEngine.XR.Interaction.Toolkit;
 /// <summary> Component that handles objects that can be spawned over detected marks. </summary>
 public class Spawnable : MonoBehaviour
 {
-    [SerializeField] private float positionLerpSpeed = 10000f;
-    [SerializeField] private float rotationLerpSpeed = 10000f;
+    [SerializeField] private float positionLerpSpeed  = 10000f;
+    [SerializeField] private float rotationLerpSpeed  = 10000f;
+    [SerializeField] private float dockMultiplier     = 1.5f;
+    [SerializeField] private float disableTime        = 1f;
 
     private bool _isGrabbed  = false;
     private bool _withinDock = true;
-
     private MarkerInfo _marker;
+    private float _lastSeen  = 0f;
+    private Rigidbody _rb;
+
+    void Awake()
+    {
+        _rb = GetComponent<Rigidbody>(); 
+    }
 
     public void SetIsGrabbed(bool value)
     {
@@ -20,25 +28,35 @@ public class Spawnable : MonoBehaviour
     }
 
     private void checkDockStatus() {
-        _withinDock = Vector3.Distance(transform.position, _marker.Pose.position) < _marker.Size;
+        _withinDock = Vector3.Distance(transform.position, _marker.Pose.position) < _marker.Size * dockMultiplier;
     }
 
     /// <summary> Updates the GameObject's transform using the provided marker information smoothly. </summary>
     /// <param name="markerInfo"> Information regarding the marker that carries this Spawnable. </param>
     public void UpdateTransform(MarkerInfo markerInfo)
     {
+        _lastSeen = 0f;
+        gameObject.SetActive(true); 
+
         if (!_isGrabbed) {
 
             if (_withinDock) {
-                transform.position   = Vector3.Lerp(transform.position, markerInfo.Pose.position, Time.deltaTime * positionLerpSpeed);
-                transform.rotation   = Quaternion.Slerp(transform.rotation, markerInfo.Pose.rotation, Time.deltaTime * rotationLerpSpeed);
+                _rb.MovePosition(Vector3.Lerp(transform.position, markerInfo.Pose.position, Time.deltaTime * positionLerpSpeed));
+                _rb.MoveRotation(Quaternion.Slerp(transform.rotation, markerInfo.Pose.rotation, Time.deltaTime * rotationLerpSpeed));
                 transform.localScale = Vector3.one * markerInfo.Size;
             } else {
                 checkDockStatus();
             }
-
         }
-
-        this._marker = markerInfo;
+        _marker   = markerInfo;
     }
+
+    private void Update()
+    {
+        _lastSeen += Time.deltaTime;
+
+        if (!_isGrabbed && _withinDock && _lastSeen >= disableTime)
+            gameObject.SetActive(false); 
+    }
+
 }
